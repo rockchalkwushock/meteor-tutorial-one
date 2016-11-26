61e39c5ef73aa51be056515dba55b324eb0b9c67
Cody Brunner <rockchalkwushock@icloud.com>
2016-11-25 18:06:43 -0600

    Added events.

--- main.js
+++ main.js
@@ -4,11 +4,29 @@ if (Meteor.isClient) {
   // Template Helpers
   Template.main.helpers({
     todos: function(){
-      return Todos.find();
+      return Todos.find({}, { sort: { createdAt: -1 } });
     }
-  })
-}
+  });
+  Template.main.events({
+    'submit .new-todo': function(event) {
+      // Prevent refresh of page.
+      event.preventDefault();
+      let text = event.target.text.value;
 
-if (Meteor.isServer) {
-
+      Todos.insert({
+        text,
+        createdAt: new Date()
+      });
+      // clear form
+      text = '';
+    },
+    'click .toggle-checked': function() {
+      Todos.update(this._id, { $set: { checked: ! this.checked } });
+    },
+    'click .delete-todo': function() {
+      if (confirm('Are you sure?')) {
+        Todos.remove(this._id);
+      }
+    }
+  });
 }
