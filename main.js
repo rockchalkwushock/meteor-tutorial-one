9f8f7865e5ce5be356cd206977645a26812bb7f8
Cody Brunner <rockchalkwushock@icloud.com>
2016-11-25 18:34:54 -0600

    Added publish & subscribe methods on Server & Client sides.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 Todos = new Mongo.Collection('todos');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('todos');
   // Template Helpers
   Template.main.helpers({
     todos: function(){
@@ -32,7 +33,21 @@ if (Meteor.isClient) {
   });
 }
 
-// Meteor Methods
+if (Meteor.isServer) {
+  Meteor.publish('todos', function() {
+    if (!this.userId) {
+      return Todos.find();
+    } else {
+      return Todos.find({userId: this.userId});
+    }
+  })
+}
+
+
+
+
+
+// Meteor Methods: Accessible by both Client & Server
 Meteor.methods({
   addTodo: function(text) {
     if (!Meteor.userId()) {
@@ -46,9 +61,17 @@ Meteor.methods({
     });
   },
   deleteTodo: function(todoId) {
+    let todo = Todos.findOne(todoId);
+    if (todo.userId !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
     Todos.remove(todoId);
   },
   setChecked: function(todoId, setChecked) {
+    let todo = Todos.findOne(todoId);
+    if (todo.userId !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
     Todos.update(todoId, { $set: { checked: setChecked } });
   }
 });
